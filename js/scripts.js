594ab4ba5f0436883ec1d6b8cfb560479195e898
Michael Popp <michaelvonpopp@icloud.com>
2021-07-31 11:51:20 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".carousel").carousel({ interval: 2000 });
-    $("#carouselButton").click(function () {
+    $("#carouselButton").on("click", function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -10,5 +10,11 @@ $(function () {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+    $("#reserveButton").on("click", function() {
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").on("click", function() {
+        $("#loginModal").modal("show");
     });
 });
